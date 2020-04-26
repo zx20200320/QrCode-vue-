8a4e24de9ca0587c6b8173ae9ee2ac9d37d87c01
于是乎 <1466496096@qq.com>
2020-04-26 09:39:11 +0800

    添加项目文件。

---
+++ QrCode/Controllers/ORCodeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QrCode.Controllers
+{
+    public class ORCodeController : Controller
+    {
+        // GET: ORCode
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetORImage(string content)
+        {
+            string timeStr = DateTime.Now.ToFileTime().ToString();
+            Bitmap bitmap = QRCodeOp.QRCodeEncoderUtil(content);
+
+            string fileName = Server.MapPath("~") + "Content\\Images\\QRImages\\" + timeStr + ".jpg";
+
+            if (!Directory.Exists(Path.Combine(Server.MapPath("~"), "\\Content\\Images\\QRImages\\")))
+            {
+                Directory.CreateDirectory(Path.Combine(Server.MapPath("~"), "\\Content\\Images\\QRImages\\"));
+            }
+
+            bitmap.Save(fileName);//保存位图
+            string imageUrl = "/Content/Images/QRImages/" + timeStr + ".jpg";//显示图片  
+            return Content(imageUrl);
+        }
+
+        public ActionResult GetORImageContent(string imageName)
+        {
+            string fileUrl = Server.MapPath("~") + "Content\\Images\\QRImages\\" + imageName;
+            Bitmap bitMap = new Bitmap(fileUrl);
+            string content = QRCodeOp.QRCodeDecodeUtil(bitMap);
+
+            return Content(content);
+        }
+
+    }
+}
---
+++ QrCode/QRCodeOp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Web;
+using ThoughtWorks.QRCode.Codec;
+using ThoughtWorks.QRCode.Codec.Data;
+
+namespace QrCode
+{
+    public class QRCodeOp
+    {
+        /// <summary>
+        /// 生成二维码
+        /// </summary>
+        /// <param name="qrCodeContent"></param>
+        /// <returns></returns>
+        public static Bitmap QRCodeEncoderUtil(string qrCodeContent)
+        {
+            QRCodeEncoder qrCodeEncoder = new QRCodeEncoder();
+            qrCodeEncoder.QRCodeVersion = 0;
+            Bitmap bitmap  = qrCodeEncoder.Encode(qrCodeContent, Encoding.UTF8);
+
+            return bitmap;
+        }
+
+        /// <summary>
+        /// 解析二维码
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
+        public static string QRCodeDecodeUtil(Bitmap bitmap)
+        {
+            QRCodeDecoder decoder = new QRCodeDecoder();
+            string decodeString = decoder.decode(new QRCodeBitmapImage(bitmap), Encoding.UTF8);
+
+            return decodeString;
+        }
+
+    }
+}
